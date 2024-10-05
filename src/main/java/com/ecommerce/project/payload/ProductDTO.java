d2fa8bb8dbba55883b5a2b223522e204c9b30b1c
p21z0 <pagunuranprim0@gmail.com>
2024-10-06 06:57:52 +0800

    Prepared files for product module

---
+++ src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -0,0 +1,17 @@
+package com.ecommerce.project.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO {
+    private Long productId;
+    private String productName;
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double specialPrice;
+}
